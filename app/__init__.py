8e368276791ee3c679ec057338a1f0297cbe19a3
Ishant1603 <ishantwalkey@gmail.com>
2025-05-22 18:32:17 +0530

    small tweak

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ import os
 file_path = os.path.join(os.getcwd(), 'todo.db')
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'f3b9c6d2e8e84a3e9a1f2c07a9e3b4d7'
+app.config['SECRET_KEY'] = ''
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + file_path
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
