710396408dd68197c69ecffced0d30cf2f1bf3b7
Ishant1603 <ishantwalkey@gmail.com>
2025-05-31 14:37:08 +0530

    mistake5

--- app/routes.py
+++ app/routes.py
@@ -176,14 +176,3 @@ def Delete(CompleteId):
     return redirect('/completed')
 
 
-
-# @app.route('/drop_old_fk', methods=['GET'])
-# def drop_old_foreign_key():
-#     try:
-#         db.session.execute(text("""
-#             ALTER TABLE completed_todos DROP CONSTRAINT IF EXISTS completed_todos_sno_fkey;
-#         """))
-#         db.session.commit()
-#         return jsonify({"status": "success", "message": "Foreign key constraint dropped."})
-#     except Exception as e:
-#         return jsonify({"status": "error", "message": str(e)})
